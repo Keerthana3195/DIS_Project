6183d6b5e7d4af2774f56a91d966e23e26f4c3e8
Aishwarya Kulkarni <aishkulkarni13@gmail.com>
2021-11-23 23:01:40 -0500

    New Web site making code

--- DIS_Project/Controllers/HomeController.cs
+++ DIS_Project/Controllers/HomeController.cs
@@ -6,23 +6,60 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+
 
 namespace DIS_Project.Controllers
 {
     public class HomeController : Controller
     {
+        SqlCommand command = new SqlCommand();
+        SqlConnection connection = new SqlConnection();
+        SqlDataReader dr;
+        List<Food> food = new List<Food>();
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            connection.ConnectionString = "Data Source=DESKTOP-HQCSK8E;Initial Catalog=Food_Drug;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         }
 
         public IActionResult Index()
         {
-            return View();
+            FetchFoodData();
+            return View(food);
         }
+        private void FetchFoodData()
+        {
+            try
+            {
+                connection.Open();
+                command.Connection = connection;
+                //command.CommandText = "SELECT TOP(1000)[city],[state],[country],[classification],[voluntary_mandated],[distribution_pattern],[reason_for_recall],[recall_initiation_date],[Product] FROM [Food_Drug].[dbo].[Food$]"; ;
+                command.CommandText = "SELECT TOP(1000)[Product],[recall_initiation_date],[classification],[reason_for_recall],[voluntary_mandated],[country],[city],[state],[distribution_pattern] FROM [Food_Drug].[dbo].[Food$]";
+                dr = command.ExecuteReader();
+                while (dr.Read())
+                {
+                    food.Add(new Food() { Product = dr["Product"].ToString()
+                        ,Recall = dr["recall_initiation_date"].ToString(),
+                        Classification = dr["classification"].ToString(),
+                        Reason = dr["reason_for_recall"].ToString(),
+                        Mandate_Recall = dr["voluntary_mandated"].ToString(),
+                        Country = dr["country"].ToString(),
+                        City = dr["city"].ToString(),
+                        State = dr["state"].ToString(),
+                        Distribution = dr["distribution_pattern"].ToString()
 
+                    });
+                }
+                connection.Close();
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ DIS_Project/Models/Food.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIS_Project.Models
+{
+    public class Food
+    {
+        public string Product { get; set; }
+        public string Recall { get; set; }
+        public string Classification { get; set; }
+        public string Reason { get; set; }
+        public string Mandate_Recall { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Distribution { get; set; }
+    }
+}
